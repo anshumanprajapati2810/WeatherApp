c83f4d0838c27f2bc7effee55d86ef327e768667
anshumanprajapati2810 <22it003@charusat.edu.in>
2023-10-05 14:03:19 +0530

    forecast frame updated

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 let weather = {
     "apikey" : "a6b08a1dc29854430ffaff30ae40ed92",
+    "apikey2" : "18dca91f987c71d828645113346e2f32",
     fetchWeather: function(city)
     {
+        
         fetch("https://api.openweathermap.org/data/2.5/weather?q="
         + city 
         +"&units=metric&appid="
         + this.apikey
         )
-        .then((response) => response.json())
-        .then((data) => this.displayWeather(data));
+        .then((response) => {
+            if (!response.ok) {
+              throw new Error('City not found');
+            }
+            return response.json();
+          })
+          .then((data) => this.displayWeather(data))
+          .catch((error) => this.handleError(error));
     },
     displayWeather: function(data){
         const {name} = data;
@@ -16,7 +24,7 @@ let weather = {
         const {icon,description} = data.weather[0];
         const {temp,temp_min,temp_max,humidity} = data.main;
         const{speed} = data.wind; 
-        console.log(name,icon,description,temp,temp_min,temp_max,humidity,country,speed);
+        //console.log(name,icon,description,temp,temp_min,temp_max,humidity,country,speed);
         document.querySelector(".city").innerText = "Weather in " + name + " , " + country;
         document.querySelector(".icon").src= "https://openweathermap.org/img/wn/" + icon + ".png";
         document.querySelector(".description").innerText = description;
@@ -28,10 +36,43 @@ let weather = {
         document.querySelector(".weather").classList.remove("loading");
         document.body.style.backgroundImage = "url('https://source.unsplash.com/1920x1080/?"+ description + "')"
     },
+
+    fetchWeatherForecast: function(city) {
+        fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=metric&cnt=4&appid=" + this.apikey2)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('City not found');
+                }
+                return response.json();
+            })
+            .then((data) => this.displayForecast(data))
+            .catch((error) => this.handleError(error));
+    },
+
+   
+
+
+    handleError: function(error) {
+        if (error.message === 'City not found') {
+          // Handle the specific "city not found" error here
+          console.error('City not found:', error);
+          // Display an error message to the user, e.g., by updating a UI element.
+          document.querySelector(".weather").classList.remove("loading");
+          document.querySelector(".weather").innerText = "City not found. Please check the city name and try again.";
+        } else {
+          // Handle other types of errors
+          console.error('Error:', error);
+          // Display a general error message to the user
+          document.querySelector(".weather").classList.remove("loading");
+          document.querySelector(".weather").innerText = "An error occurred while fetching weather data.";
+        }
+    },
+
     search : function(){
         this.fetchWeather( document.querySelector(".search-bar").value);
-    }
+    },
 };
+
 document.querySelector(".search button").addEventListener("click", function(){
     weather.search();
 });
@@ -40,4 +81,5 @@ document.querySelector(".search-bar").addEventListener("keyup", function(event){
         weather.search();
     }
 });
-weather.fetchWeather("Anand");
+
+//weather.fetchWeather("Anand");
